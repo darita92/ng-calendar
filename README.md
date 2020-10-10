9255b2a1e82e07b52a38a3f519e2bcc35e95ee61
Daniel Arita <darita@HN-LNOHN0211m.loe.corp>
2020-10-10 13:34:53 -0600

    Change title

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JobsityCalendar
+# Simple NG Calendar
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.4.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonthComponent } from './components/month/month.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonthComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
